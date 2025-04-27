b07487f64521a2134b8a75ed19002db87e2d5182
FailHy <hylmikhalil@gmail.com>
2025-04-27 16:45:52 +0700

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ServisController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [ServisController::class, 'index'])->name('servis.index'); 
 Route::resource('servis', ServisController::class);
 Route::get('servis-dihapus', [ServisController::class, 'deletedServis'])->name('servis.deleted');
 Route::post('servis/restore/{id}', [ServisController::class, 'restore'])->name('servis.restore');
