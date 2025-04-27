b87fed0b29b2ac7114bbe4895dd4054b29b8b7a8
FailHy <hylmikhalil@gmail.com>
2025-04-27 20:28:52 +0700

    perbaikan

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\ServisController;
 
 Route::get('/', [ServisController::class, 'index'])->name('servis.index'); 
 // Menampilkan halaman utama servis
+Route::resource('servis', ServisController::class)->except(['index']);
+// Menampilkan halaman tambah, edit, dan detail servis
 Route::get('servis-dihapus', [ServisController::class, 'deletedServis'])->name('servis.deleted');
 // Menampilkan halaman servis yang dihapus
 Route::post('servis/restore/{id}', [ServisController::class, 'restore'])->name('servis.restore');
